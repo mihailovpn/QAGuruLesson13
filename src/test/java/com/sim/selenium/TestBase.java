b73c87f5f8adf8baa7c6f35cb8114545a42b8ed1
Павел Михайлов <pavel.mikhaylov@simbirsoft.com>
2021-11-20 13:25:16 +0300

    Extended TestBase

--- src/test/java/com/sim/selenium/TestBase.java
+++ src/test/java/com/sim/selenium/TestBase.java
@@ -1,7 +1,15 @@
 package com.sim.selenium;
 
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.Attachment;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class TestBase {
 
@@ -10,4 +18,21 @@ public class TestBase {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
     }
+
+    @AfterEach
+    static void afterEach() {
+        pageSource();
+        screenshotAs("LastScreenshot");
+    }
+
+    @Attachment(value = "Page source", type = "text/plain")
+    public static byte[] pageSource() {
+        return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
+    }
+
+    @Attachment(value = "{attachName}", type = "image/png")
+    public static byte[] screenshotAs(String attachName) {
+        return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+
 }
